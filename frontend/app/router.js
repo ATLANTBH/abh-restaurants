93e5423a142cceceef29f9860bfedc54939d7719
Klisura, Kenan <kenan@atlantbh.com>
2019-05-29 14:57:15 +0200

    Initial frontend app

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -1,5 +1,5 @@
-import EmberRouter from '@ember/routing/router';
-import config from './config/environment';
+import EmberRouter from "@ember/routing/router";
+import config from "./config/environment";
 
 const Router = EmberRouter.extend({
   location: config.locationType,
@@ -7,6 +7,50 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route("login");
+  this.route("register");
+  this.route("restaurants", function() {});
+
+  this.route("user", function() {
+    this.route("settings");
+  });
+
+  this.route("restaurant", { path: "restaurant/:restaurant_id" });
+
+  this.route("bad-request", { path: "*path" });
+
+  this.route("search-results");
+  this.route("admin", function() {
+    this.route("restaurants", function() {
+      this.route("new");
+      this.route("edit", { path: "edit/:restaurant_id" });
+      this.route("delete", { path: "delete/:restaurant_id" });
+      this.route("reservations", { path: "reservations/:restaurant_id" });
+    });
+
+    this.route("locations", function() {
+      this.route("new");
+      this.route("edit", { path: "edit/:location_id" });
+      this.route("delete", { path: "delete/:location_id" });
+    });
+
+    this.route("users", function() {
+      this.route("new");
+      this.route("edit", { path: "edit/:user_id" });
+      this.route("delete", { path: "delete/:user_id" });
+    });
+
+    this.route("settings");
+    this.route("cuisines", function() {
+      this.route("new");
+      this.route("edit", { path: "edit/:cuisine_id" });
+      this.route("delete", { path: "delete/:cuisine_id" });
+    });
+  });
+
+  this.route("reservation-details", {
+    path: "reservation-details/:reservation_id"
+  });
 });
 
 export default Router;
