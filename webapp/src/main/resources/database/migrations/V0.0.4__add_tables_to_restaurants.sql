f180eb43970126fb90c2d28fda3ecc0059b4b659
Vedad Fejzagic <vedad.fejzagic@atlantbh.com>
2021-07-23 17:13:07 +0200

    Add 6 people tables to each restaurant

--- webapp/src/main/resources/database/migrations/V0.0.4__add_tables_to_restaurants.sql
+++ webapp/src/main/resources/database/migrations/V0.0.4__add_tables_to_restaurants.sql
@@ -1,80 +1,107 @@
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Glass Exchange'), 2);
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Glass Exchange'), 4);
+INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Glass Exchange'), 6);
 
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Bengal Way'), 2);
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Bengal Way'), 4);
+INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Bengal Way'), 6);
 
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Caviar Junction'), 2);
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Caviar Junction'), 4);
+INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Caviar Junction'), 6);
 
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Cinnamon Room'), 2);
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Cinnamon Room'), 4);
+INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Cinnamon Room'), 6);
 
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Grand Hook'), 2);
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Grand Hook'), 4);
+INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Grand Hook'), 6);
 
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'Melting Pot'), 2);
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'Melting Pot'), 4);
+INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'Melting Pot'), 6);
 
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'Roadhouse'), 2);
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'Roadhouse'), 4);
+INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'Roadhouse'), 6);
 
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'Friends'), 2);
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'Friends'), 4);
+INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'Friends'), 6);
 
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'Crickets'), 2);
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'Crickets'), 4);
+INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'Crickets'), 6);
 
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Island'), 2);
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Island'), 4);
+INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Island'), 6);
 
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Crimson Leaf'), 2);
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Crimson Leaf'), 4);
+INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Crimson Leaf'), 6);
 
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Proud Cottage'), 2);
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Proud Cottage'), 4);
+INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Proud Cottage'), 6);
 
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Enigma Archive'), 2);
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Enigma Archive'), 4);
+INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Enigma Archive'), 6);
 
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Fresh Merchant'), 2);
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Fresh Merchant'), 4);
+INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Fresh Merchant'), 6);
 
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Modern Violin'), 2);
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Modern Violin'), 4);
+INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Modern Violin'), 6);
 
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Cellar'), 2);
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Cellar'), 4);
+INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Cellar'), 6);
 
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'Curiosity'), 2);
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'Curiosity'), 4);
+INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'Curiosity'), 6);
 
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'Mirage'), 2);
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'Mirage'), 4);
+INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'Mirage'), 6);
 
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Smile'), 2);
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Smile'), 4);
+INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Smile'), 6);
 
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Polar Paradise'), 2);
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Polar Paradise'), 4);
+INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Polar Paradise'), 6);
 
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Noodle Saloon'), 2);
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Noodle Saloon'), 4);
+INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Noodle Saloon'), 6);
 
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Vintage Archive'), 2);
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Vintage Archive'), 4);
+INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Vintage Archive'), 6);
 
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Southern Dragon'), 2);
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Southern Dragon'), 4);
+INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Southern Dragon'), 6);
 
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Harlequin Lighthouse'), 2);
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Harlequin Lighthouse'), 4);
+INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Harlequin Lighthouse'), 6);
 
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'Little Persia'), 2);
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'Little Persia'), 4);
+INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'Little Persia'), 6);
 
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'Brimstone'), 2);
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'Brimstone'), 4);
+INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'Brimstone'), 6);
 
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Emperor'), 2);
 INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Emperor'), 4);
+INSERT INTO restaurant_table (restaurant_id, number_of_chairs) VALUES ((SELECT id FROM restaurant WHERE name = 'The Emperor'), 6);
